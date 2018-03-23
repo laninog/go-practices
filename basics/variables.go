9374ec0bba0c59575cccca879c73eba223c83c56
laninog <alejandro6230@gmail.com>
2018-03-23 19:26:11 +0100

    Add variables and const samples

--- basics/variables.go
+++ basics/variables.go
@@ -1,6 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/cmplx"
+)
+
+var (
+	MaxInt uint64     = 1<<64 - 1
+	i      complex128 = cmplx.Sqrt(-5 + 12i)
+)
+
+const (
+	MyConst   = 100
+	YourConst = "You"
+)
 
 var x, y, z int = 1, 2, 3
 var c, python, java = true, false, "now"
@@ -12,4 +25,10 @@ func main() {
 	fmt.Println(x, y, z, c, python, java)
 
 	fmt.Println(cPlusPlus, javaScript)
+
+	const format = "%T(%v)\n"
+	fmt.Printf(format, MaxInt, MaxInt)
+	fmt.Printf(format, z, z)
+
+	fmt.Println(MyConst, YourConst)
 }
