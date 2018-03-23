8b25f538ebd8c59bdcf6e18722b51afbd6267927
laninog <alejandro6230@gmail.com>
2018-03-23 19:11:41 +0100

    Add multiple results function

--- basics/main.go
+++ basics/main.go
@@ -6,7 +6,15 @@ func sum(x, y int) int {
 	return x + y
 }
 
+func multipleResults(x, y string) (string, string) {
+	return y, x
+}
+
 func main() {
 	// Function basic
 	fmt.Println(sum(42, 23))
+
+	// Function multiple results
+	a, b := multipleResults("world", "hello")
+	fmt.Println(a, b)
 }
